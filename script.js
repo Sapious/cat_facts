4ac33ae81cae5d48a559b3b43bb21b85da8e30a7
raed bahri <raedbahri90@gmail.com>
2022-06-26 17:30:29 +0100

    facts ready

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const descriptionElement = document.getElementById("description");
 const imageElement = document.getElementById("cat_image");
 const fetchBtn = document.getElementById("fetch");
-
-const obj = {
-	name: "raed",
-	age: 23,
-	greeting: function () {
-		return `hello ${this.name}`;
-	},
-};
-obj.lastName = "bahri";
 async function getFact() {
 	const res = await fetch("https://cat-fact.herokuapp.com/facts/random");
 	return await res.json();
 }
-
+async function getImage() {
+	const res = await fetch("https://cataas.com/cat");
+	return res.blob();
+}
 fetchBtn.addEventListener("click", async () => {
 	const data = await getFact();
 	descriptionElement.innerText = data.text;
+    const image = await getImage();
+	imageElement.src = URL.createObjectURL(image);
 });
